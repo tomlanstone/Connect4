f78f1cd21946081093415d30e5c604337a3fd74e
Tom Lanstone <thomaslanstone@hotmail.co.uk>
2020-07-11 16:45:07 +0100

    copied over ConsoleService class

---
+++ Connect4/ConsoleService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Connect4
+{
+    public class ConsoleService
+    {
+        private static readonly string EOL = Environment.NewLine;
+        private readonly string[] yesValues = new string[] { "y", "yes", "true", "1" };
+
+        public void PrintLine(string line = "", ConsoleColor? foregroundColor = null, ConsoleColor? backgroundColor = null)
+        {
+            Print(line, foregroundColor, backgroundColor);
+            Console.WriteLine();
+        }
+
+        public void Print(string line = "", ConsoleColor? foregroundColor = null, ConsoleColor? backgroundColor = null)
+        {
+            ConsoleColor previousForegroundColor = Console.ForegroundColor;
+            if (foregroundColor != null)
+            {
+                Console.ForegroundColor = (ConsoleColor)foregroundColor;
+            }
+
+            ConsoleColor previousBackgroundColor = Console.BackgroundColor;
+            if (backgroundColor != null)
+            {
+                Console.BackgroundColor = (ConsoleColor)backgroundColor;
+            }
+
+            Console.Write(line);
+
+            Console.ForegroundColor = previousForegroundColor;
+            Console.BackgroundColor = previousBackgroundColor;
+        }
+
+        public string AskQuestion(string question, string errorMessage = "")
+        {
+            Print(EOL + errorMessage + EOL + EOL + question + EOL + "--> ");
+            return Console.ReadLine();
+        }
+
+        public bool AskYesNo(string question)
+        {
+            string answer = AskQuestion(question);
+            return yesValues.Contains(answer.ToLower());
+        }
+
+        public void SetBackgroundColor(ConsoleColor color)
+        {
+            Console.BackgroundColor = color;
+        }
+
+        public void SetForegroundColor(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+        }
+
+        public void ResetConsoleColors()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            ClearScreen();
+        }
+
+        public void ClearScreen()
+        {
+            Console.Clear();
+        }
+
+        public void PrintPlayerColor(string value, Player player)
+        {
+            if (player == Player.One)
+            {
+                Print(value, ConsoleColor.Blue);
+                return;
+            }
+
+            Print(value, ConsoleColor.Red);
+        }
+    }
+}
--- Connect4/Program.cs
+++ Connect4/Program.cs
@@ -4,7 +4,44 @@ namespace Connect4
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string V = "";
+        private static readonly string EOL = Environment.NewLine;
+
+    private static readonly ConsoleService consoleService = new ConsoleService();
+
+    //private static readonly string[] boardTemplate = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+    //private static readonly string[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+    private static int boardLength = 1;
+
+    private static readonly string[] yesOptions = { "y", "yes" };
+
+    private static readonly string[] noOptions = { "n", "no" };
+
+    private static bool isP1 = true;
+
+    private static bool gameOver = false;
+
+    private static bool stalemate = false;
+
+    private static bool playAgain = true;
+
+    private static string player1 = "Player 1";
+
+    private static string player2 = "Player 2";
+
+    private static string winnerShape = "";
+
+    private static string playAgainAnswer = string.Empty;
+
+    private static int player1Score = 0;
+
+    private static int player2Score = 0;
+
+    private static int stalemateCount = 0;
+    {
+        static void Main()
         {
             Console.WriteLine("Hello World!");
         }
